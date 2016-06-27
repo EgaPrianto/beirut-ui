de7a45f39e861eedf36e09348d5c888076025c4a
Fahrizal Septrianto <fahrizal.septrianto@gdn-commerce.com>
2016-06-27 17:27:10 +0700

    confused

---
+++ src/main/java/com/gdn/x/ui/beirut/controller/BeirutController.java
@@ -0,0 +1,9 @@
+package com.gdn.x.ui.beirut.controller;
+
+@Controller
+@RequestMapping(value = BeirutController.BASE_PATH)
+public class BeirutController {
+
+	public static final String BASE_PATH = "/beirut/api";
+	
+}
