56f98fb64df38085f2821b64462ff601cef6b1a6
Fahrizal Septrianto <fahrizal.septrianto@gdn-commerce.com>
2016-06-28 11:24:02 +0700

    commit #2

--- src/main/java/com/gdn/x/ui/beirut/service/BeirutService.java
+++ src/main/java/com/gdn/x/ui/beirut/service/BeirutService.java
@@ -1,15 +1,22 @@
 package com.gdn.x.ui.beirut.service;
 
-import org.springframework.data.domain.Pageable;
-
-import com.gdn.common.web.wrapper.response.GdnBaseRestResponse;
 import com.gdn.common.web.wrapper.response.GdnRestListResponse;
 import com.gdn.common.web.wrapper.response.GdnRestSingleResponse;
+import com.gdn.x.beirut.dto.response.CandidateDTOResponse;
 import com.gdn.x.beirut.dto.response.PositionDTOResponse;
 
 public interface BeirutService {
-	
-	GdnRestListResponse<PositionDTOResponse> getAllPositions(String requestId,
-		      String username, Pageable pageable) throws Exception;
+
+  GdnRestListResponse<CandidateDTOResponse> findCandidateByCreatedDateBetweenAndStoreId(
+      String requestId, String username, Long start, Long end, int page, int size) throws Exception;
+
+  GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
+      String requestId, String username, int page, int size) throws Exception;
+
+  GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
+      String requestId, String username, String emailAddress) throws Exception;
+
+  GdnRestListResponse<PositionDTOResponse> getAllPositionWithPageable(String requestId,
+      String username, int page, int size) throws Exception;
 
 }
