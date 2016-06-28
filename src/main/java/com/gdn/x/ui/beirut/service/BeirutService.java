7680094fa19f7f4d45eb676fe148d05589230738
Ega Prianto <ega.prianto@gdn-commerce.com>
2016-06-28 13:09:46 +0700

    nambahin insertSamaDelete

--- src/main/java/com/gdn/x/ui/beirut/service/BeirutService.java
+++ src/main/java/com/gdn/x/ui/beirut/service/BeirutService.java
@@ -1,22 +1,38 @@
 package com.gdn.x.ui.beirut.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.gdn.common.web.wrapper.response.GdnBaseRestResponse;
 import com.gdn.common.web.wrapper.response.GdnRestListResponse;
 import com.gdn.common.web.wrapper.response.GdnRestSingleResponse;
+import com.gdn.x.beirut.dto.request.ListStringRequest;
+import com.gdn.x.beirut.dto.request.PositionDTORequest;
 import com.gdn.x.beirut.dto.response.CandidateDTOResponse;
 import com.gdn.x.beirut.dto.response.PositionDTOResponse;
 
 public interface BeirutService {
 
-  GdnRestListResponse<CandidateDTOResponse> findCandidateByCreatedDateBetweenAndStoreId(
+  public GdnBaseRestResponse deleteCandidate(String requestId, String username,
+      ListStringRequest idsRequest) throws Exception;
+
+  public GdnBaseRestResponse deletePosition(String requestId, String username,
+      ListStringRequest idsToDelete) throws Exception;
+
+  public GdnRestListResponse<CandidateDTOResponse> findCandidateByCreatedDateBetweenAndStoreId(
       String requestId, String username, Long start, Long end, int page, int size) throws Exception;
 
-  GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
+  public GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
       String requestId, String username, int page, int size) throws Exception;
 
-  GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
+  public GdnRestSingleResponse<CandidateDTOResponse> findCandidateByEmailAddressAndStoreId(
       String requestId, String username, String emailAddress) throws Exception;
 
-  GdnRestListResponse<PositionDTOResponse> getAllPositionWithPageable(String requestId,
+  public GdnRestListResponse<PositionDTOResponse> getAllPositionWithPageable(String requestId,
       String username, int page, int size) throws Exception;
 
+  public GdnBaseRestResponse insertNewCandidate(String requestId, String username,
+      String candidateDTORequestString, MultipartFile file) throws Exception;
+
+  public GdnRestSingleResponse<PositionDTOResponse> insertNewPosition(String requestId,
+      String username, PositionDTORequest positionDTORequest) throws Exception;
 }
--- src/main/java/com/gdn/x/ui/beirut/service/impl/BeirutServiceImpl.java
+++ src/main/java/com/gdn/x/ui/beirut/service/impl/BeirutServiceImpl.java
@@ -4,10 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.gdn.common.web.wrapper.response.GdnBaseRestResponse;
 import com.gdn.common.web.wrapper.response.GdnRestListResponse;
 import com.gdn.common.web.wrapper.response.GdnRestSingleResponse;
 import com.gdn.x.beirut.clientsdk.BeirutApiClient;
+import com.gdn.x.beirut.dto.request.ListStringRequest;
+import com.gdn.x.beirut.dto.request.PositionDTORequest;
 import com.gdn.x.beirut.dto.response.CandidateDTOResponse;
 import com.gdn.x.beirut.dto.response.PositionDTOResponse;
 import com.gdn.x.ui.beirut.service.BeirutService;
@@ -20,9 +24,35 @@ public class BeirutServiceImpl implements BeirutService {
   private BeirutApiClient beirutApiClient;
 
   @Override
+  public GdnBaseRestResponse deleteCandidate(String requestId, String username,
+      ListStringRequest idsRequest) throws Exception {
+    GdnBaseRestResponse gdnResponse =
+        this.beirutApiClient.deleteCandidate(requestId, username, idsRequest);
+    if (!gdnResponse.isSuccess()) {
+      LOG.warn(
+          "failed to findCandidateByCreatedDateBetweenAndStoreId with requestId:{} errorCode:{} errorMessage:{}",
+          requestId, gdnResponse.getErrorCode(), gdnResponse.getErrorMessage());
+    }
+    return gdnResponse;
+  }
+
+  @Override
+  public GdnBaseRestResponse deletePosition(String requestId, String username,
+      ListStringRequest idsToDelete) throws Exception {
+    GdnBaseRestResponse gdnResponse =
+        this.beirutApiClient.deletePosition(requestId, username, idsToDelete);
+    if (!gdnResponse.isSuccess()) {
+      LOG.warn(
+          "failed to findCandidateByCreatedDateBetweenAndStoreId with requestId:{} errorCode:{} errorMessage:{}",
+          requestId, gdnResponse.getErrorCode(), gdnResponse.getErrorMessage());
+    }
+    return gdnResponse;
+  }
+
+  @Override
   public GdnRestListResponse<CandidateDTOResponse> findCandidateByCreatedDateBetweenAndStoreId(
       String requestId, String username, Long start, Long end, int page, int size)
-          throws Exception {
+      throws Exception {
     GdnRestListResponse<CandidateDTOResponse> gdnCandidateDTOResponses = this.beirutApiClient
         .findCandidateByCreatedDateBetweenAndStoreId(requestId, username, start, end, page, size);
     if (!gdnCandidateDTOResponses.isSuccess()) {
@@ -67,6 +97,33 @@ public class BeirutServiceImpl implements BeirutService {
           gdnPositionDTOResponses.getErrorMessage());
     }
     return gdnPositionDTOResponses;
+  }
+
+  @Override
+  public GdnBaseRestResponse insertNewCandidate(String requestId, String username,
+      String candidateDTORequestString, MultipartFile file) throws Exception {
+    GdnBaseRestResponse gdnResponse = this.beirutApiClient.insertNewCandidate(requestId, username,
+        candidateDTORequestString, file);
+    if (!gdnResponse.isSuccess()) {
+      LOG.warn(
+          "failed to findCandidateByCreatedDateBetweenAndStoreId with requestId:{} errorCode:{} errorMessage:{}",
+          requestId, gdnResponse.getErrorCode(), gdnResponse.getErrorMessage());
+    }
+    return gdnResponse;
+  }
+
+  @Override
+  public GdnRestSingleResponse<PositionDTOResponse> insertNewPosition(String requestId,
+      String username, PositionDTORequest positionDTORequest) throws Exception {
+    GdnRestSingleResponse<PositionDTOResponse> gdnPositionDTOResponse =
+        this.beirutApiClient.insertNewPosition(requestId, username, positionDTORequest);
+    if (!gdnPositionDTOResponse.isSuccess()) {
+      LOG.warn(
+          "failed to findCandidateByCreatedDateBetweenAndStoreId with requestId:{} errorCode:{} errorMessage:{}",
+          requestId, gdnPositionDTOResponse.getErrorCode(),
+          gdnPositionDTOResponse.getErrorMessage());
+    }
+    return gdnPositionDTOResponse;
   }
 
 }
